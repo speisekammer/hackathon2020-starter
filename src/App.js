1d38d65979ee82b1896b5d82d83e90fa55d82986
Matthias Schmid <mail@matthias-schmid.de>
2020-11-08 22:00:05 +0100

    implement pantry overview app with some example data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import items from "./ExampleData.js";
 
+// render an item
+const Item = (item) => (
+  <div style={{clear: 'both'}}>
+    <img src={item.productImageUrl} alt="ProductImage" style={{margin: 5, float: 'left'}} />
+    {item.contentString()}{' '}
+    {item.name}<br />
+    <ul>
+    {item.itemSet.map(set => {
+      console.log(set)
+      return <li>{`${set.count}x with date ${set.date.toISOString().slice(0,10).replace(/-/g,"/")}`}</li>
+    })}
+    </ul>
+    
+  </div>
+);
+
+// Main App
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Pantry Overview</h1>{" "}
+        <ul style={{  listStyleType: 'none'}}>
+          <li>{items.map((item) => Item(item))}</li>
+        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/ExampleData.js
@@ -0,0 +1,57 @@
+// Definition of an item. This definition should be used if it can be used in your solution
+class Item  {
+    constructor(gtin, name, productImageUrl, content, unit) {
+        this.gtin = gtin;
+        this.name = name;
+        this.productImageUrl = productImageUrl
+        this.content = content
+        this.unit = unit
+        this.itemSet = []
+      }
+
+      contentString() {
+          return `${this.content} ${this.unit}`
+      }
+
+    addSet(count, date) {
+        this.itemSet.push({count, date})
+    }
+
+    getNumberOfItemSets() {
+        return this.itemSet.length
+    }
+
+    getItemSet(i) {
+        return this.itemSet[i]
+    }
+
+    clearItemSet() {
+        this.itemSet = []
+    }
+
+    
+}
+
+// Define some example products. Data is taken from openfoodfacts.org
+const items = [
+    new Item(4003586104892, "Kessel Chips Cross Cut Ranch", "https://static.openfoodfacts.org/images/products/400/358/610/4892/front_de.16.100.jpg", 120, 'g'),
+    new Item(5000328237503, "Salsa picante frasco", "https://static.openfoodfacts.org/images/products/500/032/823/7503/front_fr.41.100.jpg", 326, 'g'),
+    new Item(4029764001807, "CLUB-MATE", "https://static.openfoodfacts.org/images/products/402/976/400/1807/front_de.110.100.jpg", 500, 'ml')
+
+]
+
+// Add some expiration dates
+items[0].clearItemSet()
+items[0].addSet(5, new Date())
+items[0].addSet(2, new Date('August 19, 2019 23:15:30'))
+items[0].addSet(3, new Date('December 24, 2020 20:15:00'))
+
+items[1].clearItemSet()
+items[1].addSet(1, new Date('September 01, 2020 20:15:30'))
+
+items[2].clearItemSet()
+items[2].addSet(1, new Date('August 01, 2020 20:15:30'))
+items[2].addSet(1, new Date('March 01, 2021 20:15:30'))
+
+
+export default items
