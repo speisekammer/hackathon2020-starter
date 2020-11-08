54e2370643cd0e390dfbd81b16341f7f85486c60
Matthias Schmid <mail@matthias-schmid.de>
2020-11-08 22:05:50 +0100

    minor fixes and formatting

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import items from "./ExampleData.js";
 
 // render an item
 const Item = (item) => (
-  <div style={{clear: 'both'}}>
-    <img src={item.productImageUrl} alt="ProductImage" style={{margin: 5, float: 'left'}} />
-    {item.contentString()}{' '}
-    {item.name}<br />
-    <ul>
-    {item.itemSet.map(set => {
-      console.log(set)
-      return <li>{`${set.count}x with date ${set.date.toISOString().slice(0,10).replace(/-/g,"/")}`}</li>
-    })}
+  <div style={{ clear: "both" }}>
+    <img
+      src={item.productImageUrl}
+      alt="ProductImage"
+      style={{ margin: 5, float: "left" }}
+    />
+    {item.contentString()} {item.name}
+    <br />
+    <ul style={{ listStyleType: "none" }}>
+      {item.itemSet.map((set) => {
+        return (
+          <li>{`${set.count}x with date ${set.date
+            .toISOString()
+            .slice(0, 10)
+            .replace(/-/g, "/")}`}</li>
+        );
+      })}
     </ul>
-    
   </div>
 );
 
@@ -23,7 +30,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Pantry Overview</h1>{" "}
-        <ul style={{  listStyleType: 'none'}}>
+        <ul style={{ listStyleType: "none" }}>
           <li>{items.map((item) => Item(item))}</li>
         </ul>
         <p>
